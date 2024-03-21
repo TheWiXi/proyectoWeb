8cf74c9e9f7aa8e58df46f13af6724012d1f560d
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-03-21 11:00:53 -0500

    feat: :alembic: Se inicia aprendizaje Js
    Se agrega array con el json en el js para importarlo al HTML, se agrega funcion para que se seleccionen los botones de categorias

---
+++ js/main.js
@@ -0,0 +1,184 @@
+// PRODUCTOS
+const products = [
+    // Abrigos
+    {
+        "nombre": "Chaqueta Impermeable En Gabán Para Dama 'Azul'",
+        "imagen": "/storage/img/abrigos/01.webp",
+        "precio": 93900,
+        "id": "abrigo-01",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+    },
+    {
+        "nombre": "Chaqueta Impermeable En Gabán Para Dama 'Negro'",
+        "imagen": "/storage/img/abrigos/02.webp",
+        "precio": 93900,
+        "id": "abrigo-02",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+    },
+    {
+        "nombre": "Chaqueta Impermeable En Gabán Para Dama 'Blanco'",
+        "imagen": "/storage/img/abrigos/03.webp",
+        "precio": 93900,
+        "id": "abrigo-03",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+    },
+    {
+        "nombre": "Chaqueta Hombre Cuero Sintético",
+        "imagen": "/storage/img/abrigos/04.webp",
+        "precio": 102000,
+        "id": "abrigo-04",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+    },
+    {
+        "nombre": "Calidad Chaqueta Hombre Algodon Colombiano Buso Ropa Buzos",
+        "imagen": "/storage/img/abrigos/05.webp",
+        "precio": 77900,
+        "id": "abrigo-05",
+        categoria: {
+            nombre: "Abrigos",
+            id: "abrigos"
+        },
+    },
+    // Camisetas
+    {
+        "nombre": "Camiseta De Compresión En Licra Uv Slim.",
+        "imagen": "/storage/img/camisetas/01.webp",
+        "precio": 38000,
+        "id": "Camiseta 01",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        }
+    },
+    {
+        "nombre": "Camiseta Tipo Polo Color Dama Mujer.",
+        "imagen": "/storage/img/camisetas/02.webp",
+        "precio": 26255,
+        "id": "Camiseta 02",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        }
+    },
+    {
+        "nombre": "Camiseta Deportiva, Crossfit, Gym, Ejercicios Al Aire Libre.",
+        "imagen": "/storage/img/camisetas/03.webp",
+        "precio": 29900,
+        "id": "Camiseta 03",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        }
+    },
+    {
+        "nombre": "Camisetas Bandas De Rock Metal Heavy Riffs.",
+        "imagen": "/storage/img/camisetas/04.webp",
+        "precio": 32850,
+        "id": "Camiseta 04",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        }
+    },
+    {
+        "nombre": "Camiseta Anime Nezuko Kamado Kimetsu No Yaiba Demon Slayer",
+        "imagen": "/storage/img/camisetas/05.webp",
+        "precio": 29900,
+        "id": "Camiseta 05",
+        categoria: {
+            nombre: "Camisetas",
+            id: "camisetas"
+        }
+    },
+    // Pantalones
+    {
+        "nombre": "Pantalon En Dril Licrado Para Hombre.",
+        "imagen": "/storage/img/pantalones/01.webp",
+        "precio": 58900,
+        "id": "Pantalón 01",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        }
+    },
+    {
+        "nombre": "Pantalón Camuflado Hermoso.",
+        "imagen": "/storage/img/pantalones/02.webp",
+        "precio": 104405,
+        "id": "Pantalón 02",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        }
+    },
+    {
+        "nombre": "Pantalón Jogger Cargo, Táctico, Militar.",
+        "imagen": "/storage/img/pantalones/03.webp",
+        "precio": 104500,
+        "id": "Pantalón 03",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        }
+    },
+    {
+        "nombre": "Pantalón Jeans De Colores Entubado Para Hombre.",
+        "imagen": "/storage/img/pantalones/04.webp",
+        "precio": 61750,
+        "id": "Pantalón 04",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        }
+    },
+    {
+        "nombre": "Pantalon Camuflado, De Bolsillos 100% Dreal",
+        "imagen": "/storage/img/pantalones/05.webp",
+        "precio": 89990,
+        "id": "Pantalón 05",
+        categoria: {
+            nombre: "Pantalones",
+            id: "pantalones"
+        }
+    }
+];
+
+const productGallery = document.querySelector("#product__gallery");
+const buttonsMenu = document.querySelectorAll(".category__button");
+
+function uploadProducts(){
+    products.forEach(product=>{
+        const div = document.createElement("div");
+        div.classList.add("product")
+        div.innerHTML = `
+            <img class="product__img" src="${product.imagen}" alt="">
+            <div class="product__description">
+                <h3 class="product__tittle">${product.nombre}</h3>
+                <p class="product__cost">$${product.precio}</p>
+                <button class="product__add">Agregar</button>
+            </div>
+        `;
+        productGallery.append(div);
+    })
+}
+
+uploadProducts();
+
+buttonsMenu.forEach(button => {
+    button.addEventListener("click",(e) =>{
+        buttonsMenu.forEach(button => button.classList.remove("active"))
+        e.currentTarget.classList.add("active");
+    })
+})
